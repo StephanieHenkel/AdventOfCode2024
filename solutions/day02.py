da86568629c6faa04edbd0c186c1c6c891f04b16
Stephanie Henkel <73990574+StephanieHenkel@users.noreply.github.com>
2024-12-10 11:25:55 -0800

    puzzle A of day 2 adapted

--- solutions/day02.py
+++ solutions/day02.py
@@ -1,4 +1,5 @@
 from aocd.models import Puzzle
+from collections import Counter
 
 
 def load_data(input_data):
@@ -12,27 +13,29 @@ def solve_puzzle_a(unusual_data):
     safe_reports = 0
 
     for report in unusual_data:
-        cur_lvl_change = 0
-        old_lvl = report[0]
-        safe = True
-        for cur_lvl in report[1:]:
-            if abs(old_lvl - cur_lvl) > 3 or cur_lvl == old_lvl:
-                safe = False
-                break
-            elif cur_lvl > old_lvl:
-                if (cur_lvl_change * 1) == -1:
-                    safe = False
-                    break
-                else:
-                    cur_lvl_change = 1
-            else:
-                if (cur_lvl_change * -1) == -1:
-                    safe = False
-                    break
-                else:
-                    cur_lvl_change = -1
-            old_lvl = cur_lvl
-        if safe:
+
+        if safety_check(report):
+            safe_reports += 1
+
+    return safe_reports
+
+
+def safety_check(report):
+    differences = [report[i + 1] - report[i] for i in range(len(report) - 1)]
+
+    for diff in differences:
+        if diff < -3 or diff == 0 or diff > 3:
+            return False
+
+    return True
+
+
+def solve_puzzle_b(unusual_data):
+    #tbd
+    safe_reports = 0
+
+    for report in unusual_data:
+        if safety_check(report):
             safe_reports += 1
 
     return safe_reports
@@ -47,3 +50,8 @@ if __name__ == "__main__":
     answer_a = solve_puzzle_a(unusual_data)
     print(f"Number of Safe Reports: {answer_a}/{len(unusual_data)}")
     puzzle.answer_a = answer_a
+
+    # solve and submit puzzle b
+    answer_b = solve_puzzle_b(unusual_data)
+    print("Similarity Score: ", answer_b)
+    # puzzle.answer_b = answer_b
