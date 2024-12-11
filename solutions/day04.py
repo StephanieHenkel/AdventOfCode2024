d10d5d6c45f5daf20ee050560518e21002b57bca
Stephanie Henkel <73990574+StephanieHenkel@users.noreply.github.com>
2024-12-11 10:27:13 -0800

    day 04 puzzle A strategy changed: following every path of X's instead of regex search and matrix rotation

--- solutions/day04.py
+++ solutions/day04.py
@@ -7,49 +7,67 @@ def load_data(input_data):
 
 
 def solve_puzzle_a(words):
-    xmas_count = 0
-    xmas_count += sum(len(re.findall("XMAS", x)) for x in words) # left to right
-    xmas_count += sum(len(re.findall("XMAS", x[::-1])) for x in words) # right to left
+    total_count = 0
 
-    up_down_words = ["".join(row) for row in list(zip(*words))]
-    xmas_count += sum(len(re.findall("XMAS", x)) for x in up_down_words)  # up to down
-    xmas_count += sum(len(re.findall("XMAS", x[::-1])) for x in up_down_words) # down to up
+    for i in range(len(words[0])):
+        for j in range(len(words)):
+            if words[i][j] == "X":
+                # go up
+                total_count += move(words, i, j, match_word="XMAS", direction=go_up)
+                # go down
+                total_count += move(words, i, j, match_word="XMAS", direction=go_down)
+                # go right
+                total_count += move(words, i, j, match_word="XMAS", direction=go_right)
+                # go left
+                total_count += move(words, i, j, match_word="XMAS", direction=go_left)
+                # go right down
+                total_count += move(words, i, j, match_word="XMAS", direction=lambda i, j: go_right(*go_down(i, j)))
+                # go right up
+                total_count += move(words, i, j, match_word="XMAS", direction=lambda i, j: go_right(*go_up(i, j)))
+                # go left down
+                total_count += move(words, i, j, match_word="XMAS", direction=lambda i, j: go_left(*go_down(i, j)))
+                # go left up
+                total_count += move(words, i, j, match_word="XMAS", direction=lambda i, j: go_left(*go_up(i, j)))
 
-    to_right_diag_words = get_to_right_diag(words)
-    xmas_count += sum(len(re.findall("XMAS", x)) for x in to_right_diag_words)  # up to down
-    xmas_count += sum(len(re.findall("XMAS", x[::-1])) for x in to_right_diag_words)  # down to up
-
-    to_left_diag_words = get_to_right_diag([word[::-1] for word in words])
-    xmas_count += sum(len(re.findall("XMAS", x)) for x in to_left_diag_words)  # up to down
-    xmas_count += sum(len(re.findall("XMAS", x[::-1])) for x in to_left_diag_words)  # down to up
-    return xmas_count
+    return total_count
 
 
-def get_to_right_diag(words):
-    diagonal_words = []
-    for col in range(len(words[0]) - 4 + 1):
-        i = 0
-        j = col
-        new_row = ""
+def go_up(i, j):
+    return i - 1, j
 
-        while j < len(words[0]) and i < len(words):
-            new_row = new_row + words[i][j]
-            i += 1
-            j += 1
-        diagonal_words.append(new_row)
 
-    for row in range(1, len(words) - 4 + 1):
-        i = row
-        j = 0
-        new_row = ""
+def go_down(i, j):
+    return i + 1, j
 
-        while j < len(words[0]) and i < len(words):
-            new_row = new_row + words[i][j]
-            i += 1
-            j += 1
-        diagonal_words.append(new_row)
 
-    return diagonal_words
+def go_right(i, j):
+    return i, j + 1
+
+
+def go_left(i, j):
+    return i, j - 1
+
+
+def in_boundaries(words, i, j):
+    return 0 <= j < len(words[0]) and 0 <= i < len(words)
+
+
+def move(words, i, j, match_word="XMAS", direction=go_up):
+    count = 1
+    i, j = direction(i, j)
+
+    while in_boundaries(words, i, j) and count < len(match_word):
+        if words[i][j] != match_word[count]:
+            return 0
+        else:
+            count += 1
+            i, j = direction(i, j)
+
+    if count == len(match_word):
+        return 1
+    else:
+        return 0
+
 
 def solve_puzzle_b(left_list, right_list):
     # TBD
@@ -59,6 +77,7 @@ def solve_puzzle_b(left_list, right_list):
 def print_words(words):
     for line in words:
         print(line)
+
 
 if __name__ == "__main__":
     # get data
